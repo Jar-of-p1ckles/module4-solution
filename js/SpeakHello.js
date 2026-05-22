cb85e751335270ba160d47a78134dd0a83f808ea
Nicolas Trujillo <nicolastrujillocampos@gmail.com>
2026-05-22 11:42:43 -0600

    .

--- js/SpeakHello.js
+++ js/SpeakHello.js
@@ -1,5 +1,5 @@
 // Short hand array creation
-var names = ["Yaakov", "Paul", "Frank", "Larry", "Paula", "Laura"];
+var names = ["Yaakov", "Paul", "Frank", "Larry", "Paula", "Laura", "pj"];
 console.log(names);
 
 
